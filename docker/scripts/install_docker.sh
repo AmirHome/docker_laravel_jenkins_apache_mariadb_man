b9f984d616a3040642b0435909f4d3e1eb20a1c1
amirhome <amir.email@gmail.com>
2022-02-17 12:57:02 +0300

    Create Boot Service for AWS

--- docker/scripts/install_docker.sh
+++ docker/scripts/install_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# Server information
 lsb_release -a
+service --status-all
 
 # How To Install And Run Docker On Ubuntu 20.04 LTS
 sudo apt update
@@ -12,11 +14,16 @@ sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -a -G docker ubuntu
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
 docker -v
 docker-compose -v
+
+sudo cp -v ./docker/docker_boot.service /etc/systemd/system
+sudo systemctl enable docker_boot.service
+sudo systemctl start docker_boot.service
+sudo systemctl status docker_boot.service
 
 sudo mkdir ./docker_data/jenkins_home
 sudo chown 1000:1000 ./docker_data/ -R
